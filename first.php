ae0956a480ba41f7f6a8a8bbf4bfa206a5adaa00
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2023-08-18 14:05:16 +0300

    Commit first try

---
+++ first.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP</title>
+</head>
+<body>
+    <h1>First PHP</h1>
+
+    <?php
+    $var1 = "Hello, World!";
+    echo $var1;
+    ?>
+</body>
+</html>

e47c473d4b0efae524eefdc9c84390ea676e3b44
Ramer Alegre Ramos <ramzxs@users.noreply.github.com>
2023-09-08 11:33:39 +0300

    Lecture 20230908 1133

---
+++ mydiary/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Diary</title>
+</head>
+<body>
+    <h1>My Diary</h1>
+
+    <form action="?" method="post">
+        <table border="1" cellpadding="4" cellspacing="0">
+            <tr>
+                <td>Title:</td>
+                <td>
+                    <input type="text" name="title" value="Today">
+                </td>
+            </tr>
+            <tr>
+                <td>Content:</td>
+                <td>
+                    <textarea name="body" cols="30" rows="10">Message</textarea>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <button type="save">Save</button>
+                    <button type="reset">Clear</button>
+                </td>
+            </tr>
+        </table>
+    </form>
+    <br>
+    <br>
+
+    <?php
+    if ( isset($_POST['title']) ) {
+        echo "FORM SUBMITTED<br>";
+        echo "Title = " . $_POST['title'] . '<br>';
+        echo "Content = " . $_POST['body'] . '<br>';
+
+        $diaryEntry = fopen("entries/" . $_POST['title'] . ".txt", 'w');
+        fwrite($diaryEntry, $_POST['body']);
+        echo "File written.";
+    }
+    ?>
+</body>
+</html>
--- querydatabase.php
+++ querydatabase.php
@@ -8,16 +8,41 @@
 <body>
     <h1>Query Database</h1>
     <?php
-    try {
-        $CON = new PDO("mysql:hostname=localhost;dbname=myapp_db", 'root', '');
-        $stmt = $CON->query("SELECT * FROM `user`;");
-        while ($row = $stmt->fetch()) {
-            echo $row['userID'] . ' = ' . $row['userPassword'];
-            echo "<hr>";
-        }
-    } catch(Exception $ex) {
-        echo "ERROR";
+    $CON = new PDO("mysql:hostname=localhost;dbname=myapp_db", 'root', '');
+    $stmt = $CON->query("SELECT * FROM `user`;");
+    while ( $row = $stmt->fetch() ) {
+        echo $row['userID'] . ' = ' . $row['userPassword'];
+        echo "<hr>";
     }
+    ?>
+
+
+    <h2>Insert Example</h2>
+
+    <input type="text" placeholder="Username"><br>
+    <input type="password">
+
+    <?php
+
+    $sqlInsert = "INSERT INTO `user`
+        VALUES('student', PASSWORD('learner'))";
+    $CON->exec($sqlInsert);
+    echo "User account created";
     ?>
 </body>
 </html>
+
+<?php
+/*
+echo "<hr><hr>";
+
+$loginSQL = "SELECT * FROM `user`
+     WHERE `userID` = 'ramzxs'
+     AND `userPassword` = PASSWORD('Phoenix15')";
+$stmt = $CON->query($loginSQL);
+if ($row = $stmt->fetch()) {
+    echo "Login successful";
+} else {
+    echo "Login failed";
+}
+*/
